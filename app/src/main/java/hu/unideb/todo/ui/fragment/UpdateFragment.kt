db860158e032171174062c9772de0453ba5f9116
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-10 17:50:28 +0100

    created UpdateFragment.kt

---
+++ app/src/main/java/hu/unideb/todo/ui/fragment/UpdateFragment.kt
@@ -0,0 +1,23 @@
+package hu.unideb.todo.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import hu.unideb.todo.R
+import hu.unideb.todo.databinding.FragmentMainBinding
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,
+            R.layout.fragment_update,container,false)
+
+        return binding.root;
+    }
+}
