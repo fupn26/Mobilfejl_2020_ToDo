76704a4ee3b8c8c823fdfe3908dc10ef369db051
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-10 11:10:04 +0100

    added insertAll function to ToDoDatabaseDao.kt

--- app/src/main/java/hu/unideb/todo/database/ToDoDatabaseDao.kt
+++ app/src/main/java/hu/unideb/todo/database/ToDoDatabaseDao.kt
@@ -1,16 +1,16 @@
 package hu.unideb.todo.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface ToDoDatabaseDao {
 
     @Insert
     fun insert(toDo: ToDoEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll( videos: List<ToDoEntity>)
 
     @Query("SELECT * from todo_table WHERE id = :key")
     fun findById(key: Long): ToDoEntity?
