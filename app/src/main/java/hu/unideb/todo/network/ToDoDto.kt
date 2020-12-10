179b89c26cd8182ea63bfeed48a4f57586f3e944
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-10 11:05:08 +0100

    added ToDoContainer to ToDoDto

--- app/src/main/java/hu/unideb/todo/network/ToDoDto.kt
+++ app/src/main/java/hu/unideb/todo/network/ToDoDto.kt
@@ -1,8 +1,12 @@
 package hu.unideb.todo.network
 
-import hu.unideb.todo.database.ToDoEntity
+import com.squareup.moshi.JsonClass
 import hu.unideb.todo.model.ToDoModel
 
+@JsonClass(generateAdapter = true)
+data class ToDoDtoContainer(val toDos: List<ToDoDto>)
+
+@JsonClass(generateAdapter = true)
 data class ToDoDto(
     val userId: Long,
     val id: Long,
@@ -10,8 +14,8 @@ data class ToDoDto(
     val completed: Boolean
 )
 
-fun List<ToDoDto>.asDomainModel(): List<ToDoModel> {
-    return map {
+fun ToDoDtoContainer.asDomainModel(): List<ToDoModel> {
+    return toDos.map {
         ToDoModel(
             toDoId = it.id,
             title = it.title,
