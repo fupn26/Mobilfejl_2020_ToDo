84519cc0f21f3b21d04d71007d3d8bcee316fa57
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-10 14:53:31 +0100

    created Util.kt

---
+++ app/src/main/java/hu/unideb/todo/util/Util.kt
@@ -0,0 +1,6 @@
+package hu.unideb.todo.util
+
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+
+class ToDoItemViewHolder(val cardView: MaterialCardView): RecyclerView.ViewHolder(cardView)
