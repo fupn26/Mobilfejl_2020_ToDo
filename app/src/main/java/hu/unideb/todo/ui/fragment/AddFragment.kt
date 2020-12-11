824f0f2fb20558f225d9807bf973a12996712b8e
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-11 17:26:04 +0100

    added keyboard hiding to click listeners

--- app/src/main/java/hu/unideb/todo/ui/fragment/AddFragment.kt
+++ app/src/main/java/hu/unideb/todo/ui/fragment/AddFragment.kt
@@ -1,10 +1,12 @@
 package hu.unideb.todo.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import hu.unideb.todo.R
@@ -21,12 +23,15 @@ class AddFragment : Fragment() {
 
         binding.saveButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_addFragment_to_mainFragment)
+            val inputMethodManager = this.requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
         }
 
         binding.cancelButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_addFragment_to_mainFragment)
+            val inputMethodManager = this.requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
         }
-
         return binding.root
     }
 }
--- app/src/main/java/hu/unideb/todo/ui/fragment/UpdateFragment.kt
+++ app/src/main/java/hu/unideb/todo/ui/fragment/UpdateFragment.kt
@@ -1,17 +1,22 @@
 package hu.unideb.todo.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import hu.unideb.todo.R
 import hu.unideb.todo.databinding.FragmentUpdateBinding
 
-class UpdateFragment : Fragment() {
+class UpdateFragment : Fragment(), AdapterView.OnItemSelectedListener {
+
+    private var completedStatus: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +27,15 @@ class UpdateFragment : Fragment() {
 
         binding.updateButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_updateFragment_to_mainFragment)
+            val inputMethodManager = this.requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
         }
 
         binding.updateCancelButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_updateFragment_to_mainFragment)
+            val inputMethodManager = this.requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
         }
 
         ArrayAdapter.createFromResource(
@@ -38,5 +48,13 @@ class UpdateFragment : Fragment() {
         }
 
         return binding.root;
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//        parent?.getItemIdAtPosition(position).also { completedStatus = it?.equals(1)}
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
